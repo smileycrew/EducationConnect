7ad1548b46f9b17cb0434007950dc2ea5af9345e
Edwin Moz <edwin.moz@icloud.com>
2024-07-26 17:20:36 -0500

    configured program file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,52 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.Cookie.Name = "EducationConnectLoginCookie";
+    options.Cookie.SameSite = SameSiteMode.Strict;
+    options.Cookie.HttpOnly = true;
+    options.Cookie.MaxAge = new TimeSpan(7, 0, 0, 0);
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = new TimeSpan(24, 0, 0);
+    options.Events.OnRedirectToLogin = (context) =>
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+        return Task.CompletedTask;
+    };
+    options.Events.OnRedirectToAccessDenied = (context) =>
+    {
+        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+
+        return Task.CompletedTask;
+    };
+});
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddIdentityCore<IdentityUser>(config =>
+{
+    config.Password.RequireDigit = true;
+    config.Password.RequireLowercase = true;
+    config.Password.RequireNonAlphanumeric = true;
+    config.Password.RequireUppercase = true;
+    config.Password.RequiredLength = 12;
+    config.User.RequireUniqueEmail = true;
+})
+.AddRoles<IdentityRole>();
+// .AddEntityFrameworkStores<EducationConnectDbContext>();
+// builder.Services.AddNpgsql<EducationConnectDbContext>(builder.Configuration["EducationConnectDbConnectionString"]);
 builder.Services.AddSwaggerGen();
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimespanBehavior", true);
 
 var app = builder.Build();
 
@@ -14,31 +57,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
